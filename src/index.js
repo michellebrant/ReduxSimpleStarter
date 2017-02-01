9abb395f91feed9739b47a3e63267589f56e6297
Michelle Brant <michelle.brant18@gmail.com>
2017-02-01 17:23:52 -0500

    beginning of course

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//we want to create a new component
+//this component should produce some HTML
+//this is all javascript promise
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//THIS IS A CLASS
+//IT CREATES INSTANCES OF THE COMPONENTS!!!!!!!!!!!!
+//creating an instance is really easy!
+const App = function() {
+  return <div> Hi! </div>
+  //^that is actually calling react.createElement for us
+  //component name/class, using it inside JSX
+  //is a component instance :)
+  //<app> creates an instance of it
+  //since it has nothing between <app></app>
+  //can do <App />
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//need to see how this goes in the DOM somehow
+//take this component's generated HTML and
+//put it on the page! AKA in the DOM
+//first thing that wont work:
+//we need to tell that we need react!
+ReactDOM.render(<App />);
+
+//^that now creates an instance of app that allows it to render!
