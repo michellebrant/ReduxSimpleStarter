fe57218c82edf4064436d810ae0022d28ae6f133
Michelle Brant <michelle.brant18@gmail.com>
2017-02-01 19:39:58 -0500

    add more

---
+++ src/components/search_bar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends Component {
+//define a new class called SB, and give it
+//access to all of the functioanlity that
+//react.component has!
+//basically just gives it a bunch of functionallity
+//from react library
+//then render function/method:
+  render() {
+    return <input />;
+  }
+}
+
+
+// const SearchBar = () => {
+//   return <input />
+// }
+
+export default SearchBar;
+//any file that imports search bar will get this component!
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
+const API_KEY = 'AIzaSyC3-6qXpEyG4llhDmEfew9-vjdA7mLdNlc'
 //we want to create a new component
 //this component should produce some HTML
 //this is all javascript promise
@@ -8,8 +10,15 @@ import ReactDOM from 'react-dom';
 //THIS IS A CLASS
 //IT CREATES INSTANCES OF THE COMPONENTS!!!!!!!!!!!!
 //creating an instance is really easy!
-const App = function() {
-  return <div> Hi! </div>
+//now written in ES6 (new way of
+//declaring a function)
+const App = () => {
+  return (
+      <div>
+         <SearchBar />
+      </div>
+
+    );
   //^that is actually calling react.createElement for us
   //component name/class, using it inside JSX
   //is a component instance :)
@@ -23,6 +32,7 @@ const App = function() {
 //put it on the page! AKA in the DOM
 //first thing that wont work:
 //we need to tell that we need react!
-ReactDOM.render(<App />);
+//needs a second argument to tell it where to insert the HTML
+ReactDOM.render(<App />, document.querySelector('.container'));
 
 //^that now creates an instance of app that allows it to render!
